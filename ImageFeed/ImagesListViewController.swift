34664ba20a03b70432e2c72b45b5efb83d78c8dc
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-08-15 11:59:10 +0300

    add 3 methods in extensions

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -12,18 +12,25 @@ override func viewDidLoad() {
     
     tableView.dataSource = self
     tableView.delegate = self
-        
     }
     
 }
 
 extension ImagesListViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
     
 }
 
 extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
     
 }
 
