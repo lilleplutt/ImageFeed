dbcd390684ae7d9d403070b30b05e945b67d7818
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-08-16 11:06:40 +0300

    complete method tableView(_ tableView: UITableView, cellForRowAt

--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -29,7 +29,12 @@ extension ImagesListViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        
+        guard let imageListCell = cell as? ImagesListCell else { return UITableViewCell() }
+        
+        configCell(for: imageListCell)
+        return imageListCell
     }
     
 }
