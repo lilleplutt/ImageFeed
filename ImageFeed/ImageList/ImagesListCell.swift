8263c008d0e0dc85e8a379e6348eeefc89326c2c
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-08-16 12:49:27 +0300

    add 3 iboulets

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -1,6 +1,13 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    
     static let reuseIdentifier = "ImagesListCell"
     
 }
