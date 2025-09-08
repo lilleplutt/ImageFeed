4b2f135c76d840f42151345b362b03fe33c1813e
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-08 11:55:29 +0300

    add minimumZoomScale and maximumZoomScale

--- ImageFeed/ImageList/SingleImageViewController.swift
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -21,6 +21,9 @@ final class SingleImageViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
     
     //MARK: - IBActions
