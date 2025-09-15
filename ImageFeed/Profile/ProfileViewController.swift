fde7927fdc62d8916a91c4e5a20010e26c7f3d9d
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-15 16:31:15 +0300

    add viewDidLoad in profileController

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,13 +2,10 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    //MARK: - IBOutlets
-    
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBOutlet weak var logoutButton: UIButton!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var loginNameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
     
     
     
