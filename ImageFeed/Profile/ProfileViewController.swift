f7821014b74d872369e746eadda9bb85cbd9712f
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-15 16:37:37 +0300

    add profile image and imageView

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -2,10 +2,17 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    //MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let profileImage = UIImage(named: "profile_image")
+        let imageView = UIImageView(image: profileImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
     }
+    
     
     
     
