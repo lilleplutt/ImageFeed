2dab6116451c8525905f8a1b700f3642fd331356
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-17 15:12:51 +0300

    create file with Constants

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+enum Constants {
+    static let accessKey = "1UlRB2zD6MYqUW-nM042qpaYt3SPSRlIYAyvv_ik5fE"
+    static let secretKey = "Di64El2GK2aoz1mm0ihzExLXBOyhwWLz7KGuWZKzQIw"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
+
