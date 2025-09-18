1420279c25158d1818c79771327d756c5bda3427
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-18 12:16:02 +0300

    make func setUpSceneLoginScreen with basic settings

--- ImageFeed/AuthViewController.swift
+++ ImageFeed/AuthViewController.swift
@@ -5,6 +5,19 @@ class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor(named: "YP Black iOS")
+        setUpSceneLoginScreen()
+    }
+    
+    func setUpSceneLoginScreen() {
+        let unsplashLogoImage = UIImage(named: "logo_of_unsplash")
+        let unsplashLogoImageView = UIImageView(image: unsplashLogoImage)
+        unsplashLogoImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(unsplashLogoImageView)
+        
+        
         
     }
+
+    
 }
