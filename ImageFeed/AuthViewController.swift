bd7f5cbdf3bb38446cf58691fd3037bccc12aa20
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-19 14:59:21 +0300

    new settings and constraints for logo and button

--- ImageFeed/AuthViewController.swift
+++ ImageFeed/AuthViewController.swift
@@ -14,30 +14,33 @@ class AuthViewController: UIViewController {
         let unsplashLogoImage = UIImage(named: "logo_of_unsplash")
         let unsplashLogoImageView = UIImageView(image: unsplashLogoImage)
         unsplashLogoImageView.translatesAutoresizingMaskIntoConstraints = false
+        unsplashLogoImageView.contentMode = .scaleAspectFit //new
         view.addSubview(unsplashLogoImageView)
         
         unsplashLogoImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
         unsplashLogoImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        unsplashLogoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 376).isActive = true
-        unsplashLogoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 158).isActive = true
+        //unsplashLogoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 376).isActive = true
+        unsplashLogoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true //new
+        //unsplashLogoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 158).isActive = true
+        unsplashLogoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true //new
         
-        let loginButton = UIButton.systemButton(
-            with: UIImage(named: "login_button")!,
-            target: self,
-            action: nil
-        )
+        let loginButton = UIButton(type: .system)
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginButton)
         
-        loginButton.tintColor = UIColor(named: "YP White iOS")
+        //loginButton.tintColor = UIColor(named: "YP White iOS")
+        loginButton.backgroundColor = .white //new
         loginButton.setTitle("Войти", for: .normal)
         loginButton.setTitleColor(.black, for: .normal)
         loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        loginButton.layer.cornerRadius = 16 //new
         
         loginButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
-        loginButton.widthAnchor.constraint(equalToConstant: 343).isActive = true
-        loginButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 640).isActive = true
-        loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        //loginButton.widthAnchor.constraint(equalToConstant: 343).isActive = true
+        //loginButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 640).isActive = true
+        loginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true //fix
+        loginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true //new
+        loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true //new
     }
     
 
