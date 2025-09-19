89a3d984bc4f7d2843a9648e33f7912a6f6dcb4b
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-19 15:27:32 +0300

    final fix + new func

--- ImageFeed/AuthViewController.swift
+++ ImageFeed/AuthViewController.swift
@@ -6,41 +6,39 @@ class AuthViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(named: "YP Black iOS")
-        setUpLoginScreen()
+        setUpLogo()
+        setUpLoginButton()
     }
     
-    
-    func setUpLoginScreen() {
+    func setUpLogo() {
         let unsplashLogoImage = UIImage(named: "logo_of_unsplash")
         let unsplashLogoImageView = UIImageView(image: unsplashLogoImage)
         unsplashLogoImageView.translatesAutoresizingMaskIntoConstraints = false
-        unsplashLogoImageView.contentMode = .scaleAspectFit //new
+        unsplashLogoImageView.contentMode = .scaleAspectFit
         view.addSubview(unsplashLogoImageView)
         
         unsplashLogoImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
         unsplashLogoImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
-        unsplashLogoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 376).isActive = true
-        unsplashLogoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true //new
-        //unsplashLogoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 158).isActive = true
-        unsplashLogoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true //new
-        
+        unsplashLogoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 418).isActive = true
+        unsplashLogoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        unsplashLogoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100).isActive = true
+    }
+    
+    func setUpLoginButton() {
         let loginButton = UIButton(type: .system)
         loginButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(loginButton)
         
-        //loginButton.tintColor = UIColor(named: "YP White iOS")
-        loginButton.backgroundColor = .white //new
+        loginButton.backgroundColor = .white
         loginButton.setTitle("Войти", for: .normal)
         loginButton.setTitleColor(.black, for: .normal)
         loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .bold)
-        loginButton.layer.cornerRadius = 16 //new
+        loginButton.layer.cornerRadius = 16
         
         loginButton.heightAnchor.constraint(equalToConstant: 48).isActive = true
-        //loginButton.widthAnchor.constraint(equalToConstant: 343).isActive = true
-        //loginButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 640).isActive = true
-        loginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true //fix
-        loginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true //new
-        loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true //new
+        loginButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        loginButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -90).isActive = true
     }
     
 
