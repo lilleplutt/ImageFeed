6a56412b23267754d73837fde8dc8453b0dd8050
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-22 15:29:18 +0300

    add func code

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -71,5 +71,19 @@ extension WebViewViewController: WKNavigationDelegate {
               decisionHandler(.allow)
           }
       }
+    
+    private func code(from navigationAction: WKNavigationAction) -> String? {
+        if
+            let url = navigationAction.request.url,
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value                                           
+        } else {
+            return nil
+        }
+    }
 }
 
