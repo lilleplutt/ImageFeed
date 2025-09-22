f51af7d9bed3e6136ae27ad0023921871934323a
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-22 14:53:24 +0300

    add func loadAuthView()

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -7,8 +7,7 @@ class WebViewViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        loadAuthView()
     }
     
     //MARK: - Constants
@@ -20,4 +19,20 @@ class WebViewViewController: UIViewController {
     //MARK: - Outlets
     
     @IBOutlet private var webView: WKWebView!
+    
+    //MARK: - Private methods
+    
+    private func loadAuthView() {
+        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else { return }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        
+        guard let url = urlComponents.url else { return }
+    }
+    
 }
