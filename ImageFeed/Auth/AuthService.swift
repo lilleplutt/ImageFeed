44fcde83ece98a169ad26b46d11cdc9e7d60ac45
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-26 15:10:16 +0300

    add  func fetchOAuthToken

--- ImageFeed/Auth/AuthService.swift
+++ ImageFeed/Auth/AuthService.swift
@@ -21,5 +21,31 @@ final class OAuth2Service {
         request.httpMethod = "POST"
         return request
     }
+    
+    private enum NetworkError: Error {
+        case codeError
+    }
+    
+    func fetchOAuthToken(code: String, url: URL, handler: @escaping (Result<Data, Error>) -> Void) {
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                handler(.failure(error))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse,
+                response.statusCode < 200 || response.statusCode >= 300 {
+                handler(.failure(NetworkError.codeError))
+                return
+            }
+            
+            guard let data = data else { return }
+            handler(.success(data))
+        }
+        task.resume()
+    }
+    
 }
 
