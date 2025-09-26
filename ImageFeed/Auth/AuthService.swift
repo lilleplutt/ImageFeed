23b391596d897346b026ff5cca8b0bec26ec7e20
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-26 13:41:32 +0300

    make file with class OAuth2Service

---
+++ ImageFeed/Auth/AuthService.swift
@@ -0,0 +1,8 @@
+import UIKit
+import WebKit
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    private init() {}
+}
+
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -87,7 +87,6 @@ final class WebViewViewController: UIViewController {
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
     }
     
