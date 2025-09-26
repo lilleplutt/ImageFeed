8b0cd2e95976a5750d08464163e6b8a107eb7fcf
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-26 15:01:48 +0300

    remove func makeOAuthTokenRequest

--- ImageFeed/Auth/AuthService.swift
+++ ImageFeed/Auth/AuthService.swift
@@ -3,6 +3,23 @@ import WebKit
 
 final class OAuth2Service {
     static let shared = OAuth2Service() //static provide global access and uniqueness
-    private init() {} //make single exemple 
+    private init() {} //make single exemple
+    
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token") else { return nil }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let authTokenUrl = urlComponents.url else { return nil }
+        var request = URLRequest(url: authTokenUrl)
+        request.httpMethod = "POST"
+        return request
+    }
 }
 
