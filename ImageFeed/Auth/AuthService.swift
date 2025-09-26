a289b14f9973faa61493da598dd2ec66135032bf
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-26 14:37:49 +0300

    add func makeOAuthTokenRequest

--- ImageFeed/Auth/AuthService.swift
+++ ImageFeed/Auth/AuthService.swift
@@ -2,7 +2,7 @@ import UIKit
 import WebKit
 
 final class OAuth2Service {
-    static let shared = OAuth2Service()
-    private init() {}
+    static let shared = OAuth2Service() //static provide global access and uniqueness
+    private init() {} //make single exemple 
 }
 
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -73,6 +73,23 @@ final class WebViewViewController: UIViewController {
         webView.load(request)
     }
     
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token") else { return nil }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let authTokenUrl = urlComponents.url else { return nil }
+        var request = URLRequest(url: authTokenUrl)
+        request.httpMethod = "POST"
+        return request
+    }
+    
     //MARK: - KVO
     
     override func viewWillAppear(_ animated: Bool) {
