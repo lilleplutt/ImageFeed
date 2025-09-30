5ef772b6c5c0e9a515bb00ddf8be5e707db4f1fe
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-09-30 11:49:02 +0300

    add extension SplashViewController

--- ImageFeed/SplashScreen/SplashViewController.swift
+++ ImageFeed/SplashScreen/SplashViewController.swift
@@ -22,3 +22,18 @@ final class SplashViewController: UIViewController {
 
     //MARK: - Extensions
 
+extension SplashViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showAuthenticationScreenSegueIdentifier {
+            guard
+                let navigationController = segue.destination as? UINavigationController,
+                let viewController = navigationController.viewControllers.first as? AuthViewController
+            else { return }
+            
+            viewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
