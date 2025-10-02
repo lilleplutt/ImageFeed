f32c7661a10d048d6d63acac3e624e7b4469f79a
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-02 11:49:40 +0300

    save unwrapping in constants

--- ImageFeed/Constants and Models/Constants.swift
+++ ImageFeed/Constants and Models/Constants.swift
@@ -5,6 +5,10 @@ enum Constants {
     static let secretKey = "Di64El2GK2aoz1mm0ihzExLXBOyhwWLz7KGuWZKzQIw"
     static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
     static let accessScope = "public+read_user+write_likes"
-    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+    static let defaultBaseURL: URL = {
+        guard let url = URL(string: "https://api.unsplash.com") else {
+            preconditionFailure("Invalid Unsplash API base URL")
+        }
+        return url
+    }()
 }
-
