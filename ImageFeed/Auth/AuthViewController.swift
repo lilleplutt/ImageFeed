dea5357b275eb8d786b45c43ce5a2477365e8ccf
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-02 17:44:28 +0300

    fixed func webViewViewController

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -76,26 +76,25 @@ final class AuthViewController: UIViewController {
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         print("[AuthViewController] Code: \(code)")
-        vc.dismiss(animated: true)
         
         OAuth2Service.shared.fetchOAuthToken(code: code) { [weak self] result in
             DispatchQueue.main.async {
+                
+                guard let self else { return }
                 switch result {
                 case .success(let token):
                     print("[AuthViewController] Token: \(token)")
-                    self?.dismiss(animated: true) {
-                        self?.delegate?.didAuthenticate(self!)
-                    }
-                    
-                case .failure(let error):
-                    print("[AuthViewController] Error: \(error.localizedDescription)")
-                }
+                    self.delegate?.didAuthenticate(self)
+                
+            case .failure(let error):
+                print("[AuthViewController] Error: \(error.localizedDescription)")
             }
         }
     }
+}
     
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
-        dismiss(animated: true)
+        navigationController?.popViewController(animated: true)
     }
     
 }
