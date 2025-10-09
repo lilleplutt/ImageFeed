cbf74f45c373c72c2de589f5c3e5ccc32eb6e6ef
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-09 13:43:57 +0300

    update models

--- ImageFeed/Services/ProfileService.swift
+++ ImageFeed/Services/ProfileService.swift
@@ -1,31 +1,24 @@
 import UIKit
 
 //MARK: - Models
-struct ProfileResult: Codable {
-    let first_name: String
-    let last_name: String
-    let username: String
-    let bio: String
-}
-
 struct Profile {
     let username: String
     let name: String
     let loginName: String
-    let bio: String
-    
-    init(username: String, name: String, loginName: String, bio: String) {
-        self.username = username
-        self.name = name
-        self.loginName = loginName
-        self.bio = bio
-    }
-    
-    init(result: ProfileResult) {
-        self.username = result.username
-        self.name = "\(result.first_name) \(result.last_name)"
-        self.loginName = "@\(result.username)"
-        self.bio = result.bio
+    let bio: String?
+}
+
+struct ProfileResult: Codable {
+    let username: String
+    let firstName: String
+    let lastName: String
+    let bio: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case username
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case bio
     }
 }
 
