101536dfbd0664608744980949c0a9664faaefcc
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-13 11:50:23 +0300

    fix URLSession+objectTask

--- ImageFeed/Extensions/URLSession+objectTask.swift
+++ ImageFeed/Extensions/URLSession+objectTask.swift
@@ -6,17 +6,27 @@ extension URLSession {
         completion: @escaping (Result<T, Error>) -> Void
     ) -> URLSessionTask {
         let decoder = JSONDecoder()
-        let task = data(for: request) { result in
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let task = data(for: request) { (result: Result<Data, Error>) in
             switch result {
             case .success(let data):
-                do {
-                    let taskResult = try decoder.decode(T.self, from: data)
-                    completion(.success(taskResult))
-                } catch {
-                    print("[objectTask] Decoding error: \(error), Data: \(String(data: data, encoding: .utf8) ?? "")")
-                    completion(.failure(NetworkError.decodingError(error)))
+                if let jsonString = String(data: data, encoding: .utf8) {
+                    print("Get JSON: \(jsonString)")
                 }
+                do {
+                    let decodedObject = try decoder.decode(T.self, from: data)
+                    completion(.success(decodedObject))
+                } catch {
+                    if let decodingError = error as? DecodingError {
+                        print("[objectTask] Decoding Error: \(decodingError), Data: \(String(data: data, encoding: .utf8) ?? "")")
+                    } else {
+                        print("[objectTask] Decoding Error: \(error.localizedDescription), Data: \(String(data: data, encoding: .utf8) ?? "")")
+                    }
+                    completion(.failure(error))
+                }
+                
             case .failure(let error):
+                print("[objectTask] Request Error: \(error.localizedDescription)")
                 completion(.failure(error))
             }
         }
