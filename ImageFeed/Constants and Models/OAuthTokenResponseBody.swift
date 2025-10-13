ae3a250a23db5cf9594dd4b16e7d98c349988f19
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-13 12:21:34 +0300

    fix OAuthTokenResponseBody

--- ImageFeed/Constants and Models/OAuthTokenResponseBody.swift
+++ ImageFeed/Constants and Models/OAuthTokenResponseBody.swift
@@ -1,16 +1,11 @@
-import UIKit
+import Foundation
 
 struct OAuthTokenResponseBody: Decodable {
     let accessToken: String
     let tokenType: String
+    let refreshToken: String?
     let scope: String
     let createdAt: Int
-    
-    enum CodingKeys: String, CodingKey {
-        case accessToken = "access_token"
-        case tokenType = "token_type"
-        case scope
-        case createdAt = "created_at"
-    }
+    let userId: Int?
+    let username: String?
 }
-
