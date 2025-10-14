da8e35f43b0bb6280851a485b45c51fb0c7342dc
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-14 17:58:31 +0300

    add func awakeFromNib in tabBarCont

--- ImageFeed/TabBar/TabBarController.swift
+++ ImageFeed/TabBar/TabBarController.swift
@@ -1,5 +1,13 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
-    
+    override func awakeFromNib() {
+        super.awakeFromNib() // = viewDidload
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        let profileViewController = storyboard.instantiateViewController(withIdentifier: "ProfileViewController")
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
 }
