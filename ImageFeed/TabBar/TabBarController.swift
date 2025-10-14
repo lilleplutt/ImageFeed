c00615827faf97a8e8c486138ad3436d9e435984
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-14 18:09:22 +0300

    add tabBarItems for 2 controllers

--- ImageFeed/TabBar/TabBarController.swift
+++ ImageFeed/TabBar/TabBarController.swift
@@ -7,7 +7,16 @@ final class TabBarController: UITabBarController {
         let storyboard = UIStoryboard(name: "Main", bundle: .main)
         
         let imagesListViewController = storyboard.instantiateViewController(withIdentifier: "ImagesListViewController")
+        imagesListViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_editorial_active"),
+            selectedImage: nil)
+        
         let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(
+            title: "",
+            image: UIImage(named: "tab_profile_active"),
+            selectedImage: nil)
         self.viewControllers = [imagesListViewController, profileViewController]
     }
 }
