5599178c66e7be3256ae9d7eac33c0d052c7ac60
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-31 15:22:27 +0300

    add them in WebViewControllerProtocol

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -4,6 +4,8 @@ import WebKit
 public protocol WebViewControllerProtocol: AnyObject {
     var presenter: WebViewPresenterProtocol? { get set }
     func load(request: URLRequest)
+    func setProgressValue(_ newValue: Float)
+    func setProgressHidden(_ isHidden: Bool)
 }
 
 final class WebViewViewController: UIViewController, WebViewControllerProtocol {
