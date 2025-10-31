df2f63456bbe571a47f632811578345cda34ea05
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-10-31 15:15:59 +0300

    delete them from WebViewViewController

--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -17,7 +17,6 @@ final class WebViewViewController: UIViewController, WebViewControllerProtocol {
         setUpWebView()
         
         webView.navigationDelegate = self
-        loadAuthView()
         
         //MARK: - KVO
         estimatedProgressObservation = webView.observe(
@@ -59,21 +58,6 @@ final class WebViewViewController: UIViewController, WebViewControllerProtocol {
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
-    }
-    
-    private func loadAuthView() { //show authorization screen
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else { return }
-        
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        
-        guard let url = urlComponents.url else { return }
-        let request = URLRequest(url: url)
-        webView.load(request)
     }
     
     //MARK: - Methods
