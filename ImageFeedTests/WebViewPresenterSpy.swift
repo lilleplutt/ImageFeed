7643ea108d07539a34600998e1b8d90b06788b7d
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-11-01 18:04:36 +0300

    add spy and test for webCont

---
+++ ImageFeedTests/WebViewPresenterSpy.swift
@@ -0,0 +1,21 @@
+import ImageFeed
+import Foundation
+import WebKit
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    
+    var loadAuthViewCalled: Bool = false
+    var view: WebViewControllerProtocol?
+    
+    func loadAuthView() {
+        loadAuthViewCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from navigationAction: WKNavigationAction) -> String? {
+        return nil
+    }
+}
--- ImageFeedTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests.swift
@@ -2,4 +2,20 @@ import XCTest
 @testable import ImageFeed
 
 final class WebViewTests: XCTestCase {
+    
+    func testViewControllerCallsLoadAuthView() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.loadAuthViewCalled)
+    }
+    
 }
