04e2976ea552758044af4ffc0d2e621095d15b2c
Alina Turlaeva <alin.alin.alin2000@yandex.ru>
2025-11-03 15:57:43 +0300

    test feed + identifier for like and back(singleImage)

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -38,8 +38,31 @@ final class ImageFeedUITests: XCTestCase {
         XCTAssertTrue(cell.waitForExistence(timeout: 5))
     }
     
-    func testFeed() {
+    func testFeed() throws {
+        let tablesQuery = app.tables
         
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        cell.swipeUp()
+        
+        sleep(2)
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        
+        cellToLike.buttons["like button off"].tap()
+        cellToLike.buttons["like button on"].tap()
+        
+        sleep(2)
+        
+        cellToLike.tap()
+        
+        sleep(2)
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        image.pinch(withScale: 3, velocity: 1)
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navBackButtonWhiteButton = app.buttons["nav back button white"]
+        navBackButtonWhiteButton.tap()
     }
     
     func testProfile() {
